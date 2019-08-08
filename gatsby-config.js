e27060ace7d0f4965c14f659d0cd338d17b8bc30
johnathanhuggett <johnathan.huggett@amount.com>
2019-08-08 18:00:32 -0400

    cleared all lint errors

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteMetadata: {
+    title: `Bicycle Bar`,
+    description: ``,
+    author: `Bicycle Bar of Chattanooga`
+  },
+  plugins: [`gatsby-plugin-react-helmet`, 'gatsby-plugin-eslint', 'gatsby-plugin-sass']
+}
