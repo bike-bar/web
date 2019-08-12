665cb272d500673c7a7aca9cd43af9a00291877f
johnathanhuggett <johnathan.huggett@amount.com>
2019-08-12 10:25:14 -0400

    added picture component

--- src/components/utilities/picture.js
+++ src/components/utilities/picture.js
@@ -2,8 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 /*
-IMAGE ARR STRUCTURE
-[[small-1x, small-2x], [lg-1x, lg-2x], atl tag]
+  IMAGE ARR STRUCTURE
+    [
+      [small-1x, small-2x], [lg-1x, lg-2x], atl tag]
+    [
 */
 
 const Picture = ({ classNameArr, imageArr, minWidth }) => (
@@ -22,7 +24,7 @@ const Picture = ({ classNameArr, imageArr, minWidth }) => (
 Picture.propTypes = {
   classNameArr: PropTypes.array.isRequired,
   imageArr: PropTypes.array.isRequired,
-  minWidth: PropTypes.string.isRequired
+  minWidth: PropTypes.string.isRequired // ex: "(min-width: 70.02em)"
 }
 
 export default CardPicture
