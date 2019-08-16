31a7ca6be59c23b8dd6f313be5514253e8c6ca1f
Shaun <shaun37k@gmail.com>
2019-08-16 18:05:48 -0400

    disable require defaultProps per line instead

--- src/components/utilities/picture.js
+++ src/components/utilities/picture.js
@@ -16,6 +16,7 @@ const Picture = ({ classNameArr, imageArr, minWidth }) => (
       <source srcSet={`${imageArr[0][0]} 1x, ${imageArr[0][1]} 2x`} />
 
       {/* IE does not support picture tag, this is the fall back */}
+
       <img src={`${imageArr[1][0]}`} alt={imageArr[2]} />
     </picture>
   </div>
