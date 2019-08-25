0faedd772156f34b606573111bed716c66ab6fe0
Shaun <shaun37k@gmail.com>
2019-08-25 16:26:30 -0400

    spruce up footer

--- src/components/layout/footer/index.js
+++ src/components/layout/footer/index.js
@@ -5,9 +5,30 @@ const Footer = () => {
     <footer className="footer">
       <div className="container__inner">
         <div className="footer__top">
-          <div className="footer__map">map data</div>
-          <div className="footer__hours">hours</div>
-          <div className="footer__contact">contact</div>
+          <div className="footer__content">map data</div>
+          <div className="footer__content">
+            <p className="footer__header">Hours</p>
+            <div className="footer__content-row">
+              <p>Monday</p>
+              <p>6-10pm</p>
+            </div>
+            <div className="footer__content-row">
+              <p>Tueday-Thursday</p>
+              <p>5-10pm</p>
+            </div>
+            <div className="footer__content-row">
+              <p>Friday and Saturday</p>
+              <p>5pm-12am</p>
+            </div>
+          </div>
+          <div className="footer__content">
+            <p className="footer__header">Contact</p>
+            <div className="footer__content-row">
+              <p>info@thebicyclebar.com</p>
+              <p>(423) 475-6569</p>
+              <p>45 E Main St, Chattanooga, TN 37408</p>
+            </div>
+          </div>
         </div>
         <div className="footer__bottom">
           <div className="footer__social">social</div>
