8089cf72a6e5b0b75c230ba730ad61dc247a2fb7
johnathanhuggett <johnathan.huggett@amount.com>
2019-08-27 20:18:27 -0400

    changed arrows to 2

--- src/components/heros/homeHero.js
+++ src/components/heros/homeHero.js
@@ -14,9 +14,9 @@ const HomeHero = () => (
     <div className="hero__arrow hero__arrow--2">
       <HandleBars width="100px" height="100px" />
     </div>
-    <div className="hero__arrow hero__arrow--3">
+    {/* <div className="hero__arrow hero__arrow--3">
       <HandleBars width="100px" height="100px" />
-    </div>
+    </div> */}
   </div>
 )
 
