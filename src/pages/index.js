0833f90ff1a5300d782268b34b69c1261d26fb84
johnathanhuggett <johnathan.huggett@amount.com>
2019-09-11 08:41:19 -0400

    added header

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ const IndexPage = () => {
       <main ref={dropInHeader} className="landing">
         <div className="container__inner">
           <section className="instagram">
-            <SectionHeading className="instagram__heading section-heading" text="our shanaganes" />
+            <SectionHeading className="instagram__heading section-heading" text="Our Shanaganes" />
           </section>
           <Instagram />
         </div>
