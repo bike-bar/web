e642997a57e5f32006fd285b4f4a82b5bc1eb131
Shaun Kelly <shaun37k@gmail.com>
2020-02-20 17:03:03 -0500

    quick dirty event page setup plus FB plugin attempt, broken, revisit

---
+++ src/components/utilities/facebookEvents.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const FacebookEvents = () => (
+  <>
+    <div
+      className="fb-page"
+      data-href="https://www.facebook.com/TheBicycleBar/"
+      data-tabs="events"
+      data-width="250"
+      data-height="180"
+      data-small-header="false"
+      data-adapt-container-width="false"
+      data-hide-cover="false"
+      data-show-facepile="false"
+    >
+      <blockquote cite="https://www.facebook.com/TheBicycleBar/" className="fb-xfbml-parse-ignore">
+        <a href="https://www.facebook.com/TheBicycleBar/">The Bicycle Bar</a>
+      </blockquote>
+    </div>
+  </>
+)
+export default FacebookEvents
--- src/pages/events.js
+++ src/pages/events.js
@@ -1,9 +1,36 @@
 import React from 'react'
 
 import Layout from '../components/layout'
+import HomeHero from '../components/heros/homeHero'
+import { FacebookEvents, SectionHeading } from '../components/utilities'
 
-const Events = () => {
-  return <Layout>hello</Layout>
+const EventsPage = () => {
+  // setting a reference to the main content for drop in header
+  const dropInHeader = React.createRef()
+
+  /* eslint-disable consistent-return */
+  const scrollToRef = () => {
+    if (dropInHeader && dropInHeader.current) {
+      return dropInHeader.current.scrollIntoView({
+        block: 'start',
+        behavior: 'smooth',
+        inline: 'start'
+      })
+    }
+  }
+  return (
+    <Layout dropInHeader={dropInHeader}>
+      <HomeHero scrollToRef={scrollToRef} />
+      <main ref={dropInHeader} className="events">
+        <div className="container__inner">
+          <section className="facebook">
+            <SectionHeading className="facebook__heading section-heading" text="Events" />
+          </section>
+          <FacebookEvents />
+        </div>
+      </main>
+    </Layout>
+  )
 }
 
-export default Events
+export default EventsPage
